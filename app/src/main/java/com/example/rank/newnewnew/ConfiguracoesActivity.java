64757450604e7f063a76bff6655a610bd4d2745d
dierank <vinci.luiz@gmail.com>
2018-04-23 17:08:44 -0300

    add configuracoes

---
+++ app/src/main/java/com/example/rank/newnewnew/ConfiguracoesActivity.java
@@ -0,0 +1,14 @@
+package com.example.rank.newnewnew;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ConfiguracoesActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+    }
+}
