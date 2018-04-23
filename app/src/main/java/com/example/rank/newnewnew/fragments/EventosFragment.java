437c13c439657631c6d4e693f624c4346b126f5e
dierank <vinci.luiz@gmail.com>
2018-04-23 16:32:40 -0300

    update categorias/eventos/login

--- app/src/main/java/com/example/rank/newnewnew/fragments/EventosFragment.java
+++ app/src/main/java/com/example/rank/newnewnew/fragments/EventosFragment.java
@@ -1,12 +1,15 @@
 package com.example.rank.newnewnew.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.rank.newnewnew.NovoEventoActivity;
 import com.example.rank.newnewnew.R;
 
 
@@ -20,6 +23,14 @@ public class EventosFragment extends Fragment
 
         texto.setText("Fragmento: Eventos");
 
+        FloatingActionButton fab = view.findViewById(R.id.fab2);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), NovoEventoActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
